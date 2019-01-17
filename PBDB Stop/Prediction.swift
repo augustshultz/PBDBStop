21d130d3d1255d56be7da74021519259d2198f65
augustshultz <augustshultz@gmail.com>
2019-01-16 19:34:49 -0600

    Workin'

---
+++ PBDB Stop/Prediction.swift
@@ -0,0 +1,11 @@
+//
+//  Created by August Shultz on 12/31/18.
+//  Copyright © 2018 August Shultz. All rights reserved.
+//
+
+import Foundation
+
+public struct Prediction: Decodable {
+  public let name: String
+  public let minutes: Int
+}
